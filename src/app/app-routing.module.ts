abbda44db571e9bb808e85bbd309de12c9b75fd5
Guillén <guillen@MacBook-Pro-de-ideit.local>
2022-05-16 18:22:31 +0200

    Plantilla con Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
+import { ItemComponent } from './pages/item/item.component';
+import { AboutComponent } from './pages/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: PortfolioComponent },
+  { path: 'item', component: ItemComponent},
+  { path: 'about', component: AboutComponent},
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { useHash: true} ),
+  
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
