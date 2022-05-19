2bdd283a78984d1e6101be045dae501bb6ddc3a2
Guillén <guillen@MacBook-Pro-de-ideit.local>
2022-05-19 11:30:33 +0200

    Incluir JSON de base de datos en pagina

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,18 +10,31 @@ export class InfoPaginaService{
 
   info: InfoPagina ={};
   cargada=false;
+  equipo: any[] = [];
 
+  constructor (private http:HttpClient){
 
- constructor (private http:HttpClient){
-   // console.log('Servicio de infoPagina listo');
-                             
-   // Leer el archivo JSON
-        this.http.get('assets/data/data-page.json')
-        .subscribe((resp: InfoPagina )=>{
-          this.cargada=true;
-          this.info=resp;
-          console.log(resp);
-        });
+  this.cargarInfo();
+  this.cargarEquipo();
+  }
+  private cargarInfo(){
+ // Leer el archivo JSON
+  this.http.get('assets/data/data-page.json')
+  .subscribe((resp:InfoPagina)=>{
+    this.cargada=true;
+    this.info=resp;
+  });
   }
 
-}
+  private cargarEquipo() {
+    this.http.get('https://angular-html-476bd-default-rtdb.europe-west1.firebasedatabase.app/equipo.json')
+  .subscribe((resp:any)=>{
+
+    this.equipo=resp;
+    console.log(resp);
+  });
+  }
+
+
+
+ }
